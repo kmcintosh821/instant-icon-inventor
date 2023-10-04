47d8e05c614dbf0be63d65b2076b891ca6127a13
kmcintosh821 <143235216+kmcintosh821@users.noreply.github.com>
2023-10-04 00:13:27 -0400

    Creating classes for each shape + text

--- index.js
+++ index.js
@@ -1,1 +1,44 @@
 const inquirer = require('inquirer');
+const jest = require('jest');
+
+//Classes
+
+class Element {
+    constructor(elementX, elementY, color) {
+        this.elementX = elementX;
+        this.elementY = elementY;
+        this.color = color;
+    }
+}
+
+class Circle extends Element {
+    constructor(elementX, elementY, radius, color) {
+        super(elementX, elementY, color);
+        this.radius = radius;
+    }
+}
+
+class Square extends Element {
+    constructor(elementX, elementY, sideLength, cornerRoundness, color) {
+        super(elementX, elementY, color);
+        this.sideLength = sideLength;
+        this.cornerRoundness = cornerRoundness;
+    }
+}
+
+class Triangle extends Element {
+    constructor(elementX, elementY, base, height, direction, color) {
+        super(elementX, elementY, color);
+        this.base = base;
+        this.height = height;
+        this.direction = direction;
+    }
+}
+
+class Text extends Element {
+    constructor(elementX, elementY, textAnchor, fontSize, color) {
+        super(elementX, elementY, color)
+        this.textAnchor = textAnchor;
+        this.fontSize = fontSize;
+    }
+}
